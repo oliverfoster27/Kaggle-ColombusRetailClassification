2573c2783c6c7778dac54d074abfca0c2be2b157
oliverfoster27 <oliverfoster27@gmail.com>
2019-11-13 21:22:21 -0500

    update gridsearch & pipeline

--- pipeline.py
+++ pipeline.py
@@ -13,6 +13,8 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import BaggingClassifier
 from collections import namedtuple
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_selection import RFE, SelectFromModel
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -34,6 +36,7 @@ if __name__ == "__main__":
     # Defining the steps in the numerical pipeline
     numerical_pipeline = Pipeline(steps=[('num_selector', NumericalSelector()),
                                          ('imputer', SimpleImputer(strategy='median')),
+                                         # ('lrfe', LRFEPipeline()),
                                          ('std_scaler', StandardScaler())])
 
     # Combining numerical and categorical piepline into one full big pipeline horizontally
@@ -42,11 +45,14 @@ if __name__ == "__main__":
                                                    ('numerical_pipeline', numerical_pipeline)])
 
     full_pipeline = Pipeline(steps=[('prep', prep_pipeline),
-                                    ('model', BaggingClassifier(max_features=1.0, max_samples=0.2, n_estimators=200))])
+                                    # ('rfe', RFE(estimator=DecisionTreeClassifier(), n_features_to_select=1)),
+                                    ('sfm', SelectFromModel(estimator=DecisionTreeClassifier(), threshold=0)),
+                                    ('model', BaggingClassifier(max_features=1.0, max_samples=0.2,
+                                                                n_estimators=400))])
 
     full_pipeline.fit(X, y)
 
     X_test = pd.read_csv(r"C:\Users\olive\Documents\GitHub\Kaggle-ColombusRetailClassification\data\test.csv", index_col=0)
     X_test.loc[:, 'Revenue'] = ['TRUE' if x==1 else "FALSE" for x in full_pipeline.predict(X_test)]
     X_sub = X_test[['Revenue']].copy()
-    X_sub.to_csv(r"C:\Users\olive\Documents\GitHub\Kaggle-ColombusRetailClassification\experiments\e2.csv")
+    X_sub.to_csv(r"C:\Users\olive\Documents\GitHub\Kaggle-ColombusRetailClassification\experiments\e4.csv")
