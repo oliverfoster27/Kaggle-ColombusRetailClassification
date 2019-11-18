463b9fcfdff42d69e1cc39b3eb782504d1329521
oliverfoster27 <oliverfoster27@gmail.com>
2019-11-18 18:34:31 -0500

    Fixed pipeline

--- pipeline.py
+++ pipeline.py
@@ -15,11 +15,10 @@ from sklearn.ensemble import BaggingClassifier
 from collections import namedtuple
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_selection import RFE, SelectFromModel
-from sklearn.metrics import recall_score, precision_score
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
-from gridsearch import CategoricalSelector, NumericalSelector, CategoricalTransformer, f2_score
+from gridsearch import CategoricalSelector, NumericalSelector, CategoricalTransformer
 
 
 if __name__ == "__main__":
@@ -28,52 +27,32 @@ if __name__ == "__main__":
     X, y = df.drop(['Revenue'], axis=1), df['Revenue']
     y = y.apply(lambda x: 1 if x is True else 0)
 
-    prec = []
-    rec = []
-    f2 = []
-    for cv in range(1, 30):
+    # Defining the steps in the categorical pipeline
+    categorical_pipeline = Pipeline(steps=[('cat_selector', CategoricalSelector()),
+                                           ('cat_transformer', CategoricalTransformer()),
+                                           ('one_hot_encoder', OneHotEncoder(sparse=False, drop='first',
+                                                                             handle_unknown='error'))])
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    # Defining the steps in the numerical pipeline
+    numerical_pipeline = Pipeline(steps=[('num_selector', NumericalSelector()),
+                                         ('imputer', SimpleImputer(strategy='median')),
+                                         # ('lrfe', LRFEPipeline()),
+                                         ('std_scaler', StandardScaler())])
 
-        # Defining the steps in the categorical pipeline
-        categorical_pipeline = Pipeline(steps=[('cat_selector', CategoricalSelector()),
-                                               ('cat_transformer', CategoricalTransformer()),
-                                               ('one_hot_encoder', OneHotEncoder(sparse=False, drop='first',
-                                                                                 handle_unknown='error'))])
+    # Combining numerical and categorical piepline into one full big pipeline horizontally
+    # using FeatureUnion
+    prep_pipeline = FeatureUnion(transformer_list=[('categorical_pipeline', categorical_pipeline),
+                                                   ('numerical_pipeline', numerical_pipeline)])
 
-        # Defining the steps in the numerical pipeline
-        numerical_pipeline = Pipeline(steps=[('num_selector', NumericalSelector()),
-                                             ('imputer', SimpleImputer(strategy='median')),
-                                             # ('lrfe', LRFEPipeline()),
-                                             ('std_scaler', StandardScaler())])
+    full_pipeline = Pipeline(steps=[('prep', prep_pipeline),
+                                    # ('rfe', RFE(estimator=DecisionTreeClassifier(), n_features_to_select=1)),
+                                    ('sfm', SelectFromModel(estimator=DecisionTreeClassifier(), threshold=0)),
+                                    ('model', BaggingClassifier(max_features=1.0, max_samples=0.2,
+                                                                n_estimators=400))])
 
-        # Combining numerical and categorical piepline into one full big pipeline horizontally
-        # using FeatureUnion
-        prep_pipeline = FeatureUnion(transformer_list=[('categorical_pipeline', categorical_pipeline),
-                                                       ('numerical_pipeline', numerical_pipeline)])
+    full_pipeline.fit(X, y)
 
-        params = {'max_depth': 3, 'max_features': 'log2', 'n_estimators': 100}
-
-        full_pipeline = Pipeline(steps=[('prep', prep_pipeline),
-                                        # ('rfe', RFE(estimator=DecisionTreeClassifier(), n_features_to_select=1)),
-                                        ('sfm', SelectFromModel(estimator=DecisionTreeClassifier(), threshold='0.75*mean')),
-                                        ('model', GradientBoostingClassifier(**params))])
-
-        full_pipeline.fit(X_train, y_train)
-
-        y_pred = full_pipeline.predict(X_test)
-        prec.append(precision_score(y_test, y_pred))
-        rec.append(recall_score(y_test, y_pred))
-        f2.append(f2_score(y_test, y_pred))
-        # print("Precision: {}".format(prec[-1]))
-        # print("Recall: {}".format(rec[-1]))
-
-    res = pd.DataFrame(np.array([prec, rec, f2]).T)
-    print(res.loc[res[2] == res[2].median(), :])
-    print(res)
-
-    X_sub = pd.read_csv(r"C:\Users\olive\Documents\GitHub\Kaggle-ColombusRetailClassification\data\test.csv",
-                        index_col=0)
-    X_sub.loc[:, 'Revenue'] = ['TRUE' if x == 1 else "FALSE" for x in full_pipeline.predict(X_sub)]
-    X_sub = X_sub[['Revenue']].copy()
-    X_sub.to_csv(r"C:\Users\olive\Documents\GitHub\Kaggle-ColombusRetailClassification\experiments\e6_f2.csv")
+    X_test = pd.read_csv(r"C:\Users\olive\Documents\GitHub\Kaggle-ColombusRetailClassification\data\test.csv", index_col=0)
+    X_test.loc[:, 'Revenue'] = ['TRUE' if x==1 else "FALSE" for x in full_pipeline.predict(X_test)]
+    X_sub = X_test[['Revenue']].copy()
+    X_sub.to_csv(r"C:\Users\olive\Documents\GitHub\Kaggle-ColombusRetailClassification\experiments\e7.csv")
